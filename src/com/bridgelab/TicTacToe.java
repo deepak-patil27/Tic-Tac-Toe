f6c600bca339b8e90fe7b3541764247e060fd58d
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-08-07 17:45:43 +0530

    Added: Created Tic Tac Toe Game Board

--- src/com/bridgelab/TicTacToe.java
+++ src/com/bridgelab/TicTacToe.java
@@ -1,8 +1,19 @@
 package com.bridgelab;
 
 public class TicTacToe {
+
+	static char[] board = new char[10];
+
+	static void createBoard(char[] board) {
+
+		for (int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("----------Welcome To Tic Tac Toe----------");
+
 	}
 
 }
